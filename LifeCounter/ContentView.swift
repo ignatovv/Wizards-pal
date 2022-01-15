17e223ae1917b1b598ee117d493d5f501a978a94
Vladimir Ignatov <abbadoh@mail.ru>
2022-01-15 11:07:50 +0200

    Initial Commit

---
+++ LifeCounter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LifeCounter
+//
+//  Created by Vladimir Ignatov on 15.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LifeCounter/LifeCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeCounterApp.swift
+//  LifeCounter
+//
+//  Created by Vladimir Ignatov on 15.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct LifeCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
